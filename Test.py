67ea67b3407cc41ef7ae1d99523cba7f26e310f4
HiroyukiTakada-sub <71753706+HiroyukiTakada-sub@users.noreply.github.com>
2020-09-23 19:07:50 +0900

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,6 @@
-print('Hello World Python')
-print('Hello World Python')
-print('Hello World Python')
+# should insert period '.' at each last.
+# should repeat four times.
+print('Hello World Python.')
+print('Hello World Python.')
+print('Hello World Python.')
+print('Hello World Python.')
